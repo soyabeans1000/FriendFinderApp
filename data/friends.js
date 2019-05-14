79a6466c2fde49437a99b4dd972eab5ed1311368
Sarika <sarika@ngoyal.com>
2019-05-14 12:03:57 -0700

    Data updated

--- data/friends.js
+++ data/friends.js
@@ -8,7 +8,7 @@ const friendsArray = [
     {"name":"Jacob Deming","photo":"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg","scores":["4","2","5","1","3","2","2","1","3","2"]},
     {"name":"Jeremiah Scanlon","photo":"https://avatars2.githubusercontent.com/u/8504998?v=3&s=460","scores":["5","2","2","2","4","1","3","2","5","5"]},
     {"name":"Louis T. Delia","photo":"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg","scores":["3","3","4","2","2","1","3","2","2","3"]},
-    {"name":"Lou Ritter","photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg","scores":["4","3","4","1","5","2","5","3","1","4"]}
+    {"name":"Lou Ritter","photo":"https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg","scores":["4","3","4","1","5","2","5","3","1","4"]}
   
 ]
 
