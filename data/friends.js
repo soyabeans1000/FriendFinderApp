874d424e385eec3488446b8990d82b78b8dc93e5
Sarika <sarika@ngoyal.com>
2019-05-17 20:58:34 -0700

    Friend list updated

--- data/friends.js
+++ data/friends.js
@@ -4,11 +4,12 @@
 
 const friendsArray = [
   
-    {"name":"Ahmed","photo":"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg","scores":["5","1","4","4","5","1","2","5","4","1"]},
-    {"name":"Jacob Deming","photo":"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg","scores":["4","2","5","1","3","2","2","1","3","2"]},
-    {"name":"Jeremiah Scanlon","photo":"https://avatars2.githubusercontent.com/u/8504998?v=3&s=460","scores":["5","2","2","2","4","1","3","2","5","5"]},
+    {"name":"Ahmed","photo":"http://res.cloudinary.com/yaffa-publishing/image/fetch/q_auto:best,c_fill,w_300,g_faces:center,f_auto/http%3A%2F%2Fyaffa-cdn.s3.amazonaws.com%2Fadnews%2Flive%2Fimages%2FdmImage%2FSourceImage%2Fluke-achterstraat2.jpg","scores":["5","1","4","4","5","1","2","5","4","1"]},
+    {"name":"Jacob Deming","photo":"https://readytricks.com/wp-content/uploads/2018/01/happy-girl-picture-1.jpg","scores":["4","2","5","1","3","2","2","1","3","2"]},
+    {"name":"Jeremiah Scanlon","photo":"https://secure-static.tagged.com/img/browse/218997961.jpg?b9b64a1f528507e6c49fb3ee67951a949e8919a3318ac84aecf0a21d9a127c00-4","scores":["5","2","2","2","4","1","3","2","5","5"]},
     {"name":"Louis T. Delia","photo":"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg","scores":["3","3","4","2","2","1","3","2","2","3"]},
-    {"name":"Lou Ritter","photo":"https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg","scores":["4","3","4","1","5","2","5","3","1","4"]}
+    {"name":"Lou Ritter","photo":"https://celebritymanagementblog.files.wordpress.com/2018/09/kritika-kamra.jpg?w=400&h=400","scores":["4","3","4","1","5","2","5","3","1","4"]}
+  
   
 ]
 
